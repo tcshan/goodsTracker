7aa3a6e86efcf534a56bd79357b287cf8a9010e7
shan <gouwu331@gmail.com>
2020-03-24 13:13:56 -0700

    Add express server

--- app_back.js
+++ app_back.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 console.log(port);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+console.log(path.join(__dirname, 'web-build'));
+app.use(express.static(path.join(__dirname, 'web-build')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + 'web-build/index.html'));
+});
